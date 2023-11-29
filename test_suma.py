ab88c97fdd275b9504f149e87362699d08269880
gabriel melero montero <gabrix177@hotmail.com>
2023-11-29 20:14:35 +0100

    fallo en test suma

--- test_suma.py
+++ test_suma.py
@@ -4,7 +4,7 @@ from suma import sumar
 class TestStumar(unittest.TestCase):
     def test_sumar(self):
         self.assertEqual(sumar(3,2),5)
-        self.assertEqual(sumar(-1,1),5)
+        self.assertEqual(sumar(-1,1),0)
         self.assertEqual(sumar(-1,-1),-2)
 
 
